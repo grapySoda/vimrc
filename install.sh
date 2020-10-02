1411442e7f281ea88bbd92e2166823f41ec8e872
grapySoda <s910312000000@gmail.com>
2020-10-02 20:21:51 +0800

    Update install.ch

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get install curl vim-gtk build-essential cmake python3-dev -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
