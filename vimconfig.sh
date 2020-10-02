4cacc0663b1084672cd8fde804b5cdac2e00a07e
grapySoda <s910312000000@gmail.com>
2020-10-02 18:14:35 +0800

    Modify installing list

--- vimconfig.sh
+++ vimconfig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install vim-gtk build-essential cmake python3-dev -y
+sudo apt-get install curl vim-gtk build-essential cmake python3-dev -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
