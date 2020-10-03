335bbc05e328abc60af6e0fb817ee60d6d5bb782
grapySoda <s910312000000@gmail.com>
2020-10-03 18:16:31 +0800

    Modify

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo apt-get install expect curl vim-nox build-essential cmake python3-dev fonts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -E -s -u "${HOME}/.vimrc" +PlugInstall +qall
+python3 ~/.vim/plugged/YouCompleteMe/install.py --clang-completer
