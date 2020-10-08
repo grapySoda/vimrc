2b492ce6409785b5019e9b558fa31cb591f76fd7
grapySoda <s910312000000@gmail.com>
2020-10-08 18:07:23 +0800

    Modify

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev fonts-powerline -y
+sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -E -s -u "${HOME}/.vimrc" +PlugInstall +qall
