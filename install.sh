89516080bdae011ee8de9956729ef36370725317
grapySoda <s910312000000@gmail.com>
2020-10-08 18:33:28 +0800

    Modify

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev -y
-clone git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
+git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
 ~/fonts/install.sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
