09d95b8e3cf4b5521b8141d22453eb8a237d1117
grapySoda <s910312000000@gmail.com>
2020-10-08 18:20:55 +0800

    Modify

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev -y
+clone git clone https://github.com/powerline/fonts.git --depth=1 ~/fonts
+~/fonts/install.sh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -E -s -u "${HOME}/.vimrc" +PlugInstall +qall
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get autoremove vim-gtk -y
+~/fonts/uninstall.sh
+rm -rf ~/fonts
 rm -rf ~/.vim/
 rm -f ~/.vimrc
