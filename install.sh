ebcf7656039e570fbf7df7179df1cedb1e7a7791
grapySoda <68229989+grapySoda@users.noreply.github.com>
2021-08-25 13:17:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev powerline -y
+sudo apt-get install expect curl vim-gtk build-essential cmake python3-dev powerline mono-complete golang nodejs default-jdk npm -y
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
@@ -14,4 +14,5 @@ mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~/
 vim -E -s -u "${HOME}/.vimrc" +PlugInstall +qall
-python3 ~/.vim/plugged/YouCompleteMe/install.py --clang-completer
+#python3 ~/.vim/plugged/YouCompleteMe/install.py --clang-completer--cs-completer
+python3 ~/.vim/plugged/YouCompleteMe/install.py --cs-completer
